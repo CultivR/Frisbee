7eb3570b039428d1745fe0e1aea3a0458ebc09e2
Jordan Kay <jordanekay@mac.com>
2017-11-14 15:20:20 -0800

    Generate test coverage

---
+++ FrisbeeTests/FrisbeeTests.swift
@@ -0,0 +1,15 @@
+//
+//  FrisbeeTests.swift
+//  FrisbeeTests
+//
+//  Created by Jordan Kay on 11/14/17.
+//  Copyright © 2017 Cultivr. All rights reserved.
+//
+
+import XCTest
+
+class FrisbeeTests: XCTestCase {
+    func test() {
+        
+    }
+}
