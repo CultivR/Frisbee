6f8702fe72d36bad13c20e5b4bc7855717eb8f64
Jordan Kay <jordanekay@mac.com>
2018-01-19 18:00:30 -0800

    Add job title and company properties

--- Frisbee/Entities/Profile.swift
+++ Frisbee/Entities/Profile.swift
@@ -8,11 +8,15 @@
 
 public struct Profile {
     public let name: String
+    public let jobTitle: String
+    public let company: String
     public let primaryBehavior: Behavior
     public let secondaryBehavior: Behavior?
     
-    public init(name: String, primaryBehavior: Behavior, secondaryBehavior: Behavior?) {
+    public init(name: String, jobTitle: String, company: String, primaryBehavior: Behavior, secondaryBehavior: Behavior?) {
         self.name = name
+        self.jobTitle = jobTitle
+        self.company = company
         self.primaryBehavior = primaryBehavior
         self.secondaryBehavior = secondaryBehavior
     }
