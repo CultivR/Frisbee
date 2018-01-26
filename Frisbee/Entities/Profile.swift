bae40c2aabf9a987382cec437c24bc2a1e406d86
Jordan Kay <jordanekay@mac.com>
2018-01-25 18:29:55 -0800

    Make job title and company optional

--- Frisbee/Entities/Profile.swift
+++ Frisbee/Entities/Profile.swift
@@ -8,12 +8,12 @@
 
 public struct Profile {
     public let name: String
-    public let jobTitle: String
-    public let company: String
+    public let jobTitle: String?
+    public let company: String?
     public let primaryBehavior: Behavior
     public let secondaryBehavior: Behavior?
     
-    public init(name: String, jobTitle: String, company: String, primaryBehavior: Behavior, secondaryBehavior: Behavior?) {
+    public init(name: String, jobTitle: String?, company: String?, primaryBehavior: Behavior, secondaryBehavior: Behavior?) {
         self.name = name
         self.jobTitle = jobTitle
         self.company = company
