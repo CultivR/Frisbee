7be5787db941708d618c6f3921460dcea979adf2
Jordan Kay <jordanekay@mac.com>
2018-02-12 17:03:04 -0800

    Remove authentication

--- Frisbee/API/DISCAPI.swift
+++ Frisbee/API/DISCAPI.swift
@@ -9,34 +9,8 @@
 import Emissary
 import SwiftTask
 
-public typealias BasicTask = Task<Float, Void, Reason>
-public typealias DISCAPITask = Task<Float, DISCAPI, Reason>
-
-public struct DISCAPI {
-    public let user: User?
-    public let authToken: AuthToken?
-    
-    public init() {
-        user = nil
-        authToken = nil
-    }
-}
-
-extension DISCAPI: API {
-    public var authorization: Authorization? {
-        return authToken.map { .bearer(credentials: $0.accessToken)}
-    }
-    
+public struct DISCAPI: API {
     public static var baseURL: URL! {
         return Bundle.main.bundleURL
-    }
-}
-
-// TODO
-public extension Task where Progress == Float, Error == Reason {
-    static var mock: Task<Progress, Value, Error> {
-        return Task { _, fulfill, reject, _ in
-
-        }
     }
 }
