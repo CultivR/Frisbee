d3fa4a2c3d2c20a5ce3668e70f11045b02b360f5
Jordan Kay <jordanekay@mac.com>
2018-02-15 10:01:24 -0800

    Add initial profile endpoint

---
+++ Frisbee/Framework/ID.swift
@@ -0,0 +1,13 @@
+//
+//  ID.swift
+//  Frisbee
+//
+//  Created by Jordan Kay on 2/15/18.
+//  Copyright © 2018 Cultivr. All rights reserved.
+//
+
+extension Int {
+    static var randomID: Int {
+        return Int(arc4random_uniform(UInt32(Int.max)))
+    }
+}
